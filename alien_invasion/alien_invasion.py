fbbf218630127c3104b2dc4fa1d5e4bdf15435b1
yangxu199846 <1009006049@qq.com>
2022-04-02 14:51:14 +0800

    设置修改，单独存放于一个文件夹

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,13 +1,14 @@
 import sys
 import pygame
-
+from settings import Settings
 class AlienInvasion:
     '''初始化游戏并创建游戏资源'''
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((1200,800))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
-        self.bg_color = (230,230,230)
+
     def run_game(self):
         '''开始游戏的主循环'''
         while True:
@@ -16,7 +17,7 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
             #让最近绘制的屏幕可见
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
             pygame.display.flip()
 
 
